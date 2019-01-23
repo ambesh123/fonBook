bb044bd17358f41ff5170386642370f492bdd219
Ambesh Tiwari <35375583+ambesh123@users.noreply.github.com>
2019-01-23 17:01:03 +0530

    Add files via upload

---
+++ src/oracleconnect/OracleConnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracleconnect;
+
+/**
+ *
+ * @author Ambesh Tiwari
+ */
+
+public class OracleConnect {
+
+    /**
+     * @param args the command line arguments
+     */
+    static MainFrame mf;
+    public static void main(String[] args) {
+       
+        mf= new MainFrame();
+        mf.setLocation(700, 200);
+        mf.setVisible(true);
+        
+    }
+    
+}
